ca48b6bfc53820cb75566b2be7d16cb48ddaa1f0
Woo-Dong93 <tmlu48@naver.com>
2021-05-17 03:12:03 +0900

    refactor: button 컴포넌트type name 수정

--- Client/components/Button/index.tsx
+++ Client/components/Button/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './styled.scss';
 
-type TinputProps = {
+type TbuttonProps = {
   children: React.ReactNode;
   color?: 'black' | 'green' | 'blue';
   size?: 'small' | 'middle' | 'large' | 'auto';
   handler: () => void;
 };
 
-const Button: React.FC<TinputProps> = ({ children, color = 'black', size = 'auto', handler }: TinputProps) => {
+const Button: React.FC<TbuttonProps> = ({ children, color = 'black', size = 'auto', handler }: TbuttonProps) => {
   return (
     <button className={`Button ${color} ${size}`} onClick={handler}>
       {children}
